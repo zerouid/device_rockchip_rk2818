6906b4d8bbb9d6f014a4fd14da5619f7bd37dd14
FUKAUMI Naoki <naobsd@gmail.com>
2011-05-19 20:08:18 +0900

    use gralloc.rk28board derived from gralloc.default
    use RGB565 framebuffer
    override width, height, and fps with sane values

--- full_rk2818.mk
+++ full_rk2818.mk
@@ -8,7 +8,8 @@ $(call inherit-product-if-exists, vendor/rockchip/rk2818/device-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += device/rockchip/rk2818/overlay
 
 PRODUCT_PACKAGES += \
-    Gallery
+    Gallery \
+    gralloc.rk28board
 
 PRODUCT_COPY_FILES += \
     device/rockchip/rk2818/init.rc:root/init.rc
